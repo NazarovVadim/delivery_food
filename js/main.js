7d482e22219d52da733ae46f914fbc9fbec3e2b0
happy123213 <62542107+happy123213@users.noreply.github.com>
2020-05-06 14:55:11 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
@@ -26,6 +28,7 @@ let login = localStorage.getItem('notOut');
 function toggleModalAuth() {
   modalAuth.classList.toggle("is-open");
   loginInput. style = '';
+  passwordInput. style = '';
 }
 
 function autorized(){
@@ -97,3 +100,93 @@ function checkAuth(){
 checkAuth();
 
 
+//generate restaraunt 
+
+
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
+
+
+function createCardRestaraunt(){
+    const card = `
+    <a class="card card-restaurant">
+    <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image"/>
+    <div class="card-text">
+      <div class="card-heading">
+        <h3 class="card-title">Пицца плюс</h3>
+        <span class="card-tag tag">50 мин</span>
+      </div>
+      <div class="card-info">
+        <div class="rating">
+          4.5
+        </div>
+        <div class="price">От 900 ₽</div>
+        <div class="category">Пицца</div>
+      </div>
+    </div>
+  </a>`;
+
+  cardsRestaurants.insertAdjacentHTML('afterbegin', card);
+
+
+}
+createCardRestaraunt();
+
+function createCardGood(){
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  card.insertAdjacentHTML('afterbegin', `
+      <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image"/>
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title card-title-reg">Пицца Классика</h3>
+        </div>
+        <div class="card-info">
+          <div class="ingredients">Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина, салями, грибы.</div>
+        </div>
+        <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">510 ₽</strong>
+        </div>
+      </div>
+  `);
+
+  cardsMenu.insertAdjacentElement('afterbegin',card);
+}
+
+function openGoods(e){
+  const target = e.target;
+  const restaurant = target.closest('.card-restaurant');
+
+  if (restaurant && login){
+    
+    cardsMenu.textContent = '';
+
+    containerPromo.classList.add('hide');
+    restaurants.classList.add('hide');
+    menu.classList.remove('hide');
+
+    createCardGood();
+  }
+  else{
+    toggleModalAuth();
+  }
+  
+}
+
+
+
+cardsRestaurants.addEventListener('click', openGoods);
+logo.addEventListener('click', function(){
+  containerPromo.classList.remove('hide');
+  restaurants.classList.remove('hide');
+  menu.classList.add('hide');
+})
